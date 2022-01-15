69a7a774a4ebcd48844f0faa1ffc71e264e9bbcf
viobelles <96273223+viobelles@users.noreply.github.com>
2022-01-16 01:41:04 +0300

    Добавлена форма записи событий

---
+++ ProjectCalendar/События/Events.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectCalendar.События
+{
+    /// <summary>
+    /// Логика взаимодействия для Events.xaml
+    /// </summary>
+    public partial class Events : Window
+    {
+        public Events()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelBT_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult Result = MessageBox.Show("Закрыть приложение?", "qwe", MessageBoxButton.YesNo);
+            if (Result == MessageBoxResult.Yes)
+            {
+                this.Close();
+            }
+            else if (Result == MessageBoxResult.No)
+            {
+                this.Close();
+            }
+        }
+
+        private void MinButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void ExitButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ToolBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
